53062ac8e027b880528159d7871ac4ae9ae078f4
Natasha Miquele <natymiquele08@gmail.com>
2023-09-21 18:31:08 -0300

    corregir error de iconos

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const apikey = "bece4615b0b0c64eb761d4679eae9304"
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.getElementById("icon");
 
 async function checkWeather(city){
     const response = await fetch(API + city + `&appid=${apikey}`);
@@ -18,7 +19,26 @@ async function checkWeather(city){
     document.querySelector(".humedad").innerHTML = data.main.humidity;
     document.querySelector(".viento").innerHTML = data.wind.speed;
    
-    
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.innerHTML += `<i class="fa-solid fa-cloud fa-2xl"></i>`
+
+    } 
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.innerHTML += `<i class="fa-solid fa-sun fa-2xl"></i>`
+
+    } 
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.innerHTML += `<i class="fa-solid fa-cloud-rain fa-2xl"></i>`
+
+    } 
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.innerHTML += `<i class="fa-solid fa-cloud-showers-heavy fa-2xl"></i>`
+
+    } 
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.innerHTML += `<i class="fa-solid fa-smog fa-2xl"></i>`
+
+    }
 
 
 }
