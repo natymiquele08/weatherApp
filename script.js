d8b08e359b39a9ff9a85da18b2f42c38e74dea69
Natasha Miquele <natymiquele08@gmail.com>
2023-09-21 18:07:32 -0300

    primer cambio

--- script.js
+++ script.js
@@ -1,63 +1,32 @@
-/*window.addEventListener("load", ()=>{
-    let lon
-    let lat
 
-    let temperaturaValor = document.getElementsByClassName("temperaturaValor");
-    let temperaturaDescripcion = document.getElementsByClassName("temperaturaDescripcion");
-    let ubicacion = document.getElementsByClassName("ubicacion");
-    let vientoVelocidad = document.getElementsByClassName("vientoVelocidad");
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(posicion => {
-            lon = posicion.coords.longitude
-            lat = posicion.coords.latitude
-
-            const URL= `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=bece4615b0b0c64eb761d4679eae9304`
-            
-
-            fetch(URL)
-            .then( response => {return response.json()})
-            .then (data => {
-                
-                let temp = Math.round(data.main.temp)
-                temperaturaValor.textContent = temp
+const API = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apikey = "bece4615b0b0c64eb761d4679eae9304"
 
 
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-
-
-            }
-                )
-
-        })
-
-    }
-
-
-
-
-}) */
-
-
-const API = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=montevideo&appid=bece4615b0b0c64eb761d4679eae9304";
-
-
-async function checkWeather(){
-    const response = await fetch(API);
+async function checkWeather(city){
+    const response = await fetch(API + city + `&appid=${apikey}`);
     var data = await response.json();
 
     console.log(data);
 
-    document.getElementById("").innerHTML = data.name;
-    document.getElementById("").innerHTML = data.main.temp;
-    document.getElementById("").innerHTML = data.main.humidity;
-    document.getElementById("").innerHTML = data.wind.speed;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".description").innerHTML = data.weather[0].description;
+    document.querySelector(".humedad").innerHTML = data.main.humidity;
+    document.querySelector(".viento").innerHTML = data.wind.speed;
    
     
 
 
 }
-checkWeather();
+
+searchBtn.addEventListener("click", ()=> {
+    checkWeather(searchBox.value);
+})
+
 
 
 
